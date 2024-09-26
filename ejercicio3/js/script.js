754a8e2b4f15ba9a38842313e0a291ee9e6dedb9
1DAW-Jose Padilla <jpadillas03@informatica.iesvalledeljerteplasencia.es>
2024-09-26 20:57:07 +0200

    ejercicio 3

---
+++ ejercicio3/js/script.js
@@ -0,0 +1,13 @@
+/*Crea una función a la cual se le pase un array. Esta función devolverá:
+    a) Cuántos elementos hay de tipo número y los pintará por pantalla.
+    b) Cuántos elementos hay de tipo cadena y los pintará por pantalla.
+    c) Cuántos elementos no son ni número ni cadena y los pintará por pantallal.
+Prueba la ejecución con varias entradas*/
+
+function comprobar(array){
+
+    for (let i = 0; i < array.length; i++) {
+        
+        
+    }
+}
