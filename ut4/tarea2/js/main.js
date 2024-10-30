701f4a71188165479544d15468db98b45ae25c7d
1DAW-Jose Padilla <jpadillas03@informatica.iesvalledeljerteplasencia.es>
2024-10-30 15:29:45 +0100

    tarea2

--- ut4/tarea2/js/main.js
+++ ut4/tarea2/js/main.js
@@ -1,31 +1,4 @@
 /*
-<<<<<<< HEAD
-Crea un nuevo proyecto que llevará la contabilidad de un restaurante. En el restaurante habrá un array de trabajadores que tendrá un nombre, un salario/hora y un número de horas semanales. La idea de este ejercicio es implementar las clases Trabajador y Restaurante:
-
-Trabajador:
-Atributos:
-    -nombre:Nombre del trabajador. Viene como parámetro del constructor.
-    -numHorasSemanales: Horas que trabaja semanalmente. Vienen como parámetro del contructor.
-    -salarioPorHora: Euros que gana a la hora. Viene como parámetro del constructor.
-Métodos:
-    -constructor(nombre, numHorasSemanales,salarioPorHora).
-    -pintarInfo(): Muestra por pantalla las características del trabajador.
-    -getSaldoSemanal(): Devuelve lo que gana el trabajador en una semana de trabajo.
-*/
-
-class Trabajador{
-
-    constructor(nombre, numHorasSemanales, salarioPorHoras){
-        this.nombre = nombre;
-        this.numHorasSemanales = numHorasSemanales;
-        this.salarioPorHoras = salarioPorHoras;
-    }
-
-    pintarInfo(){
-        console.log("");
-    }
-}
-=======
 En el restaurante habrá un array de trabajadores que tendrán un nombre, un salario/hora, y un número de hotras semanales. La idea de este ejercicio es implementar las clases Trabajador y Restaurante:
 */
 
@@ -111,4 +84,3 @@ restaurante.annadirTrabajador(trabajador3);
 
 restaurante.pintarInfo();
 console.log("Mantener a los trabajadores del restaurante cuesta: " + restaurante.getPagosSemanales() + " euros.");
->>>>>>> 9a97692eb319ba47d4e7e42a129c2d85ec79c2de
