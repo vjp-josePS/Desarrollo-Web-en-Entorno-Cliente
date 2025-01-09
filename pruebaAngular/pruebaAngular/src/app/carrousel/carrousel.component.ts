8d6a04a574497d5e945185d7a9f78ce7af35b3b2
1DAW-Jose Padilla <jpadillas03@informatica.iesvalledeljerteplasencia.es>
2025-01-09 20:44:35 +0100

    inicio de angular hasta pag 12

---
+++ pruebaAngular/pruebaAngular/src/app/carrousel/carrousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-carrousel',
+  imports: [],
+  templateUrl: './carrousel.component.html',
+  styleUrl: './carrousel.component.scss'
+})
+export class CarrouselComponent {
+  title = 'mi primer componente con Angular';
+}
