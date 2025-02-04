70e1f88363046e037d45c9c59887836f379f0d17
1DAW-Jose Padilla <jpadillas03@informatica.iesvalledeljerteplasencia.es>
2025-02-04 17:20:25 +0100

    ns

---
+++ pruebaAngular/pruebaAngular/src/app/components/detalles-producto/detalles-producto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalles-producto',
+  imports: [],
+  templateUrl: './detalles-producto.component.html',
+  styleUrl: './detalles-producto.component.scss'
+})
+export class DetallesProductoComponent {
+
+}
---
+++ pruebaAngular/pruebaAngular/src/app/pipes/filtroProductos.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { IProducto } from '../interfaces/i-producto';
+
+@Pipe({
+  name: 'filtroProductos'
+})
+export class FiltroProductosPipe implements PipeTransform {
+
+  transform(arrayProductos: IProducto[], filtro: string): IProducto[] {
+    return arrayProductos.filter(producto=>{
+      return producto.descripcion.toLowerCase().includes(filtro.toLowerCase());
+    });
+  }
+
+}
