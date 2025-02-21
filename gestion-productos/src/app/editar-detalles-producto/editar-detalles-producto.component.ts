e9bc8ceea1a22df6e8ac4e4237a831412535d709
José Padilla Salado <114687971+vjp-josePS@users.noreply.github.com>
2025-02-21 23:57:34 +0100

    proyecto terminado sin comentar

--- gestion-productos/src/app/editar-detalles-producto/editar-detalles-producto.component.ts
+++ gestion-productos/src/app/editar-detalles-producto/editar-detalles-producto.component.ts
@@ -3,18 +3,26 @@ import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { ProductoService } from '../producto.service';
 import { IProducto } from '../i-producto/i-producto';
 import { Subscription } from 'rxjs';
-import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-editar-detalles-producto',
     standalone: true,
-    imports: [FormsModule, CommonModule, RouterModule],
+    imports: [ReactiveFormsModule, CommonModule, RouterModule],
     templateUrl: './editar-detalles-producto.component.html',
     styleUrls: ['./editar-detalles-producto.component.scss']
 })
 
 export class EditarDetallesProductoComponent implements OnInit, OnDestroy {
+    productoForm = new FormGroup({
+        descripcion: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(500)]),
+        precio: new FormControl(0, [Validators.required, Validators.pattern("^[0-9]+(\.[0-9]{1,2})?$")]),
+        disponibilidad: new FormControl(new Date(), [Validators.required]),
+        puntuacion: new FormControl(1, [Validators.required, Validators.min(1), Validators.max(5)]),
+        imagen: new FormControl(null)
+    });
+
     producto: IProducto | undefined;
     private subscription: Subscription | undefined;
     minDate: string;
@@ -36,6 +44,12 @@ export class EditarDetallesProductoComponent implements OnInit, OnDestroy {
                 this.producto = producto;
                 if (producto) {
                     this.imagenCargada = producto.imagenUrl;
+                    this.productoForm.patchValue({
+                        descripcion: producto.descripcion,
+                        precio: producto.precio,
+                        disponibilidad: producto.disponibilidad,
+                        puntuacion: producto.puntuacion
+                    });
                 }
             },
             error => console.error('Error al obtener el producto:', error)
@@ -49,29 +63,32 @@ export class EditarDetallesProductoComponent implements OnInit, OnDestroy {
     }
 
     onFileSelected(event: any): void {
-        this.selectedFile = event.target.files[0] as File;
-        this.uploadImage();
-    }
-
-    uploadImage(): void {
-        if (this.selectedFile) {
+        const file = event.target.files[0];
+        if (file) {
+            this.productoForm.get('imagen')?.setValue(file);
             const reader = new FileReader();
             reader.onload = (e: any) => {
                 this.imagenCargada = e.target.result;
             }
-            reader.readAsDataURL(this.selectedFile);
+            reader.readAsDataURL(file);
         }
     }
 
     guardarCambios(): void {
-        if (this.producto) {
-            if (this.selectedFile) {
-                this.producto.imagenUrl = this.imagenCargada || '';
-            }
-
-            this.productoService.updateProducto(this.producto).subscribe(
+        if (this.productoForm.valid && this.producto) {
+            const updatedProducto: IProducto = {
+                ...this.producto,
+                descripcion: this.productoForm.value.descripcion || '',
+                precio: this.productoForm.value.precio || 0,
+                disponibilidad: this.productoForm.value.disponibilidad || new Date(),
+                puntuacion: this.productoForm.value.puntuacion || 0,
+                imagenUrl: this.imagenCargada || ''
+            };
+    
+            this.productoService.updateProducto(updatedProducto).subscribe(
                 productoActualizado => {
                     console.log('Producto actualizado correctamente:', productoActualizado);
+                    // Navega a los detalles del producto actualizado
                     this.router.navigate(['/productos', productoActualizado.id]);
                 },
                 (error: any) => console.error('Error al actualizar el producto:', error)
--- gestion-productos/src/app/item-producto/item-producto.component.ts
+++ gestion-productos/src/app/item-producto/item-producto.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
 import { IProducto } from '../i-producto/i-producto';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterModule } from '@angular/router';
@@ -10,10 +10,17 @@ import { RouterLink, RouterModule } from '@angular/router';
   templateUrl: './item-producto.component.html',
   styleUrls: ['./item-producto.component.scss']
 })
-export class ItemProductoComponent {
+export class ItemProductoComponent implements OnChanges {
   @Input() producto: IProducto | undefined;
   @Input() mostrarImagenes: boolean = true;
   @Output() estrellasCambiadas = new EventEmitter<number>();
+  imagenUrl: string | undefined = '';
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['producto'] && this.producto) {
+      this.imagenUrl = this.producto.imagenUrl;
+    }
+  }
 
   onEstrellaClick(nuevaPuntuacion: number): void {
     this.estrellasCambiadas.emit(nuevaPuntuacion);
